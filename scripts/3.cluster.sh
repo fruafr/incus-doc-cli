13ecaae3e7d383580c4ad4f9043be11bf3377392
David Heurtevent <david.heurtevent@gmail.com>
2026-03-30 17:15:43 +0200

    Rename output files for cluster list commands
    - Fixed list, list-tokens
    - Fixed error for update-certificate

--- scripts/3.cluster.sh
+++ scripts/3.cluster.sh
@@ -8,7 +8,8 @@ incus cluster evacuate --help > ./incus/cluster/evacuate.txt
 incus cluster get --help > ./incus/cluster/get.txt
 incus cluster group --help > ./incus/cluster/group.txt
 incus cluster info --help > ./incus/cluster/info.txt
-incus cluster list --help > ./incus/cluster/list-tokens.txt
+incus cluster list --help > ./incus/cluster/list.txt
+incus cluster list-tokens --help > ./incus/cluster/list-tokens.txt
 incus cluster remove --help > ./incus/cluster/remove.txt
 incus cluster rename --help > ./incus/cluster/rename.txt
 incus cluster restore --help > ./incus/cluster/restore.txt
@@ -16,6 +17,6 @@ incus cluster revoke-token --help > ./incus/cluster/revoken-token.txt
 incus cluster role --help > ./incus/cluster/role.txt
 incus cluster set --help > ./incus/cluster/set.txt
 incus cluster show --help > ./incus/cluster/show.txt
-incus cluster update-certificates --help > ./incus/cluster/update-certificates
+incus cluster update-certificate --help > ./incus/cluster/update-certificate
 
 
